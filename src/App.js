20e8cb18f3416756f3af4fb29d89de17270f40bd
vivek9211 <82077595+vivek9211@users.noreply.github.com>
2023-01-19 18:23:32 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Welcome from "./components/welcome";
 import './App.css';
 
 
@@ -26,8 +27,9 @@ const onSubmit = () => {
         value={name}
       />
       <button onClick={onSubmit}>Login</button>
-      <h1>{fullName}</h1>
-      <h2>{collage}</h2>
+      <Welcome name={fullName} collage={collage}/>
+      {/* <h1>{fullName}</h1>
+      <h2>{collage}</h2> */}
       <p>Assignment done by Vivek Kumar Collage id - 12007412</p>
       
     </div>
